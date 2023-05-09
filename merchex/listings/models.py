538ca3c2b8bc7ebb5887578f961514c3cbf18a0d
LeoLChalot <larouchalotleo@gmail.com>
2023-05-09 10:08:51 +0200

    Modif model

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,22 +2,28 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 # listings/models.py
 
+
 class Band(models.Model):
 
     class Genre(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
+        METAL = 'M'
 
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(choices=Genre.choices, max_length=50, default='HH')
-    biography = models.fields.CharField(max_length=1000, default='', blank=True)
-    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    genre = models.fields.CharField(
+        choices=Genre.choices, max_length=50, default='HH')
+    biography = models.fields.CharField(
+        max_length=1000, default='', blank=True)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.name}'
+
 
 class Listing(models.Model):
 
@@ -31,7 +37,8 @@ class Listing(models.Model):
     description = models.fields.CharField(max_length=100, default='')
     sold = models.fields.BooleanField(default=False)
     year = models.fields.IntegerField(null=True, blank=True)
-    type = models.fields.CharField(choices=Type.choices, max_length=50, default='M')
+    type = models.fields.CharField(
+        choices=Type.choices, max_length=50, default='M')
     band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):

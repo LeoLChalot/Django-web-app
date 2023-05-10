490cfbfea66f1ce89510c1d870898d043a770d64
LeoLChalot <larouchalotleo@gmail.com>
2023-05-10 23:45:40 +0200

    Création des formulaire d'ajout

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from listings.models import Band, Listing
 
 class ContactUsForm(forms.Form):
-    name = forms.CharField(required=False)
-    email = forms.EmailField()
-    message = forms.CharField(max_length=1000)
+    name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:300px', 'class':'form-control'}), required=False)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'style':'width:300px', 'class':'form-control'}))
+    message = forms.CharField(widget=forms.TextInput(attrs={'style':'width:300px', 'class':'form-control'}),max_length=1000)
+
+class BandForm(forms.ModelForm):
+    class Meta:
+        model = Band
+        fields = '__all__'
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = '__all__'
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 from listings.models import Band
 from listings.models import Listing
 
-from listings.forms import ContactUsForm
+from listings.forms import *
 
 def home(request):
         return render(request, 'listings/home.html')
@@ -24,6 +24,17 @@ def listing_detail(request, id):
                 'listings/listing_detail.html',
                 { 'listing': listing })
 
+def listing_create(request):
+        form = ListingForm()
+        if request.method == 'POST':
+                form = ListingForm(request.POST)
+                if form.is_valid():
+                        listing = form.save()
+                        return redirect('listing-detail', listing.id)
+                else:
+                        form = ListingForm()
+        return render(request,'listings/listing_create.html', {'form': form})
+
 def contact(request):
         # print('Lam éthode de requête est : ', request.method)
         # print('Les données POST sont : ', request.POST)
@@ -32,11 +43,14 @@ def contact(request):
                 if form.is_valid():
                         send_mail(subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form', message=form.cleaned_data['message'], from_email=form.cleaned_data['email'], recipient_list=['admin@merchex.xyz'],
                         )
-                        return redirect('listings/email-sent.html')
+                        return redirect('email-sent')
         else:
                 form = ContactUsForm()
 
         return render(request, 'listings/contact.html', {'form':form})
+
+def email_sent(request):
+        return render(request, 'listings/email_confirmation.html')
 
 def band_list(request):
         bands = Band.objects.all()
@@ -49,6 +63,17 @@ def band_detail(request, id):
         return render(request,
                 'listings/band_detail.html',
                 { 'band': band })
+
+def band_create(request):
+        form = BandForm()
+        if request.method == 'POST':
+                form = BandForm(request.POST)
+                if form.is_valid():
+                        band = form.save()
+                        return redirect('band-detail', band.id)
+                else:
+                        form =BandForm()
+        return render(request, 'listings/band_create.html', {'form':form})
 
 def about(request):
         return render(request, 'listings/about.html')
